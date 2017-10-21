79417f626162dcff7c61cedb8d14364f5ef6f789
Kim Nguyen <zktnguyen@ucdavis.edu>
2017-10-20 23:38:56 -0700

    Login and logout work!

--- src/index.js
+++ src/index.js
@@ -9,8 +9,15 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './App';
 import rootReducer from './rootReducer';
 import registerServiceWorker from './registerServiceWorker';
+import { userLoggedIn } from './actions/auth';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+// This will keep the user logged in.
+if (localStorage.bookwormJWT) {
+  const user = { token: localStorage.bookwormJWT };
+  store.dispatch(userLoggedIn(user));
+}
+
 ReactDOM.render(<BrowserRouter>
   <Provider store={store}>
     <App/>
