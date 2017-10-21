b0bd95038a2e0db5d79ec50b1145a623d3beb4bf
Kim Nguyen <zktnguyen@ucdavis.edu>
2017-10-20 20:35:24 -0700

    Set up REDUX with Provider, thunk, store, middleware, composewithdevtools

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 import 'semantic-ui-css/semantic.min.css';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './rootReducer';
 
-ReactDOM.render(<BrowserRouter> 
-  <App/>
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+ReactDOM.render(<BrowserRouter>
+  <Provider store={store}>
+    <App/>
+  </Provider>
 </BrowserRouter>, document.getElementById("root"));
 registerServiceWorker();
