28fb95bfa2243f137f4aede936ec212430d12f4a
Kim Nguyen <zktnguyen@ucdavis.edu>
2017-10-20 19:18:57 -0700

    created initial home and login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Page from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
 
-const App = () => <div>Hey world</div>;
+const App = () => (
+  <div>
+    <Route path="/" exact component={Page} />
+    <Route path="/login" exact component={LoginPage} />
+  </div>
+);
 
 export default App;
